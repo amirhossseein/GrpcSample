2d1dc1252708cab163e083ede397ff1ce3667792
Ostad <Ostad@OSTAD-PC>
2023-06-03 14:29:21 +0430

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcClient;
+
+//var input = new HelloRequest { Name = "osiiiii" };
+var input = new CustomerLookupModel { UserId = 1 };
+var channel = GrpcChannel.ForAddress("https://localhost:7088");
+//var client = new Greeter.GreeterClient(channel);
+var client = new Customer.CustomerClient(channel);
+//var reply = await client.SayHelloAsync(input);
+var customer = await client.GetCustomerInfoAsync(input);
+Console.WriteLine($"{customer.FirstName}-{customer.LastName}");
+
+
+using var call = client.GetNewCustomers(new NewCustomerRequest());
+while (await call.ResponseStream.MoveNext(default))
+{
+    var currentCustomer = call.ResponseStream.Current;
+
+    Console.WriteLine($"{currentCustomer.FirstName}-{currentCustomer.LastName}-{currentCustomer.EmailAddress}");
+}
+
+
+
+Console.ReadLine();
+
+
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;   
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel outPut = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                outPut.FirstName = "ali";
+                outPut.LastName = "hoseini";
+            }
+            else if (request.UserId == 2)
+            {
+                outPut.FirstName = "amir";
+                outPut.LastName = "ostad";
+            }
+            else
+            {
+                outPut.FirstName = "hosein";
+                outPut.LastName = "haghani";
+            }
+
+            return Task.FromResult(outPut);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel> {
+                new CustomerModel
+                {
+                    FirstName = "amir",
+                    LastName = "ostad",
+                    Age = 28,
+                    EmailAddress = "something@gmail.com",
+                    IsActice = true
+                } ,
+                new CustomerModel
+                {
+                    FirstName = "hosein",
+                    LastName = "ostad",
+                    Age = 25,
+                    EmailAddress = "something2@gmail.com",
+                    IsActice = false
+                }
+            };
+
+            foreach (var item in customers)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+    }
+}
